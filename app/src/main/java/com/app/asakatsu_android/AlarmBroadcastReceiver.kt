4d6479dd82eccc5b3e9b491d34ec4479da75f5df
haruk <haruki.u@gmail.com>
2022-07-18 15:57:48 +0900

    定期的にファイル読み込む動作完了

--- app/src/main/java/com/app/asakatsu_android/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/app/asakatsu_android/AlarmBroadcastReceiver.kt
@@ -11,12 +11,22 @@ class AlarmBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         // toast で受け取りを確認
 
-        Log.d("TAG", "Applemode中はオレンジボタンは無効")
         Toast.makeText(context, "Received ", Toast.LENGTH_LONG).show()
 
 
-//        val fileName = "nextTimeInfo.txt"
+        val fileName = "nextTimeInfo.txt"
+        Log.d("■■■■■■■", "11111111111111")
+        Log.d("■■■■■■■", context.cacheDir.absolutePath)
+        if(File(context.cacheDir, fileName).exists()){
+            val text = File(context.cacheDir, fileName).readText()
+            Log.d("■■■■■■■", text)
 //        val text = File(fileName).readText()
-//        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+            Log.d("■■■■■■■", "22222222222")
+            Log.d("■■■■■■■", text)
+            Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+        }else{
+
+            Toast.makeText(context, "NOT EXISTS", Toast.LENGTH_LONG).show()
+        }
     }
 }
