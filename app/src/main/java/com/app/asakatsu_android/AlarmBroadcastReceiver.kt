e5e151d877b8bc8c85ac65bdc762b7dd6b12e313
haruk <haruki.u@gmail.com>
2022-07-18 14:38:22 +0900

    定期処理起動確認完了

--- app/src/main/java/com/app/asakatsu_android/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/app/asakatsu_android/AlarmBroadcastReceiver.kt
@@ -4,10 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import java.io.File
+import android.util.Log
 
 class AlarmBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         // toast で受け取りを確認
+
+        Log.d("TAG", "Applemode中はオレンジボタンは無効")
         Toast.makeText(context, "Received ", Toast.LENGTH_LONG).show()
+
+
+//        val fileName = "nextTimeInfo.txt"
+//        val text = File(fileName).readText()
+//        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/app/asakatsu_android/MainActivity.kt
+++ app/src/main/java/com/app/asakatsu_android/MainActivity.kt
@@ -6,24 +6,24 @@ import android.app.PendingIntent.getBroadcast
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
 import java.util.*
 
-
 //https://qiita.com/jamestong/items/50dd0977a76e83bad280
 
 class MainActivity : AppCompatActivity() {
-
+//
     private lateinit var counterLabel: TextView
     private lateinit var receivedValueLable: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        counterLabel = findViewById(R.id.textViewCount)
+        receivedValueLable = findViewById(R.id.textViewReceivedValue)
 
 //        // 時間をセットする
 //        val calendar: Calendar = Calendar.getInstance()
@@ -45,8 +45,10 @@ class MainActivity : AppCompatActivity() {
 //        // アラームをセットする
 //        val am: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 //        if (am != null) {
-//            am.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(), 5000,pending)
-////            am.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending)
+////            am.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(), 6000,pending)//60000
+////            am.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(), 5000,pending)
+//            am.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending);
+//
 //            Toast.makeText(
 //                applicationContext,
 //                "Set Alarm ", Toast.LENGTH_SHORT
@@ -65,6 +67,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        // 時間をセットする
+        val calendar: Calendar = Calendar.getInstance()
+        // Calendarを使って現在の時間をミリ秒で取得
+        calendar.setTimeInMillis(System.currentTimeMillis())
+        // 5秒後に設定
+        calendar.add(Calendar.SECOND, 1)
+
+        //明示的なBroadCast
+        val intent = Intent(
+            applicationContext,
+            AlarmBroadcastReceiver::class.java
+        )
+        val pending: PendingIntent = getBroadcast(
+            applicationContext, 0, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // アラームをセットする
+        val am: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        if (am != null) {
+//            am.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(), 6000,pending)//60000
+            am.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(), 5000,pending)
+//            am.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending);
+
+            Toast.makeText(
+                applicationContext,
+                "Set Alarm ", Toast.LENGTH_SHORT
+            ).show()
+        }
+
         val app = application as MyApplication
         if (app.count != 0) {
             counterLabel.text = "Current tap times: ${app.count}"
